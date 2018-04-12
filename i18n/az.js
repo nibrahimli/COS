0878c5e6721635ff4bec97c80a19462149beee69
Nihat Ibrahimli <nihatibrahimli@gmail.com>
2018-04-12 23:39:44 +0200

    initial commit

---
+++ i18n/az.js
@@ -0,0 +1,17 @@
+//Azerbaijan
+const texts = {
+    app_title: 'BAĞLAMA',
+    app_description: 'Baglamalarinizi dunyanin her yerine gonderin'
+  };
+  
+  const states = {
+    az: "Azərbaycanca",
+    fr: 'Fransizca',
+    en: 'Ingiliscə' 
+  };
+  
+  export default {
+    ...texts,
+    ...states
+  };
+  
---
+++ i18n/en.js
@@ -0,0 +1,17 @@
+//ENGLISH
+const texts = {
+    app_title: 'BAĞLAMA',
+    app_description: 'Send your packages all ove the world'
+  };
+  
+  const states = {
+    az: 'Azerbaidjani',
+    fr: 'French',
+    en: 'English (US)' 
+  };
+  
+  export default {
+    ...texts,
+    ...states
+  };
+  
